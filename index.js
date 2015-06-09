2f7fc2ffb7f1971c832ad79f715c0cdd6da2a424
Tom MacWright <tom@macwright.org>
2015-06-09 18:02:10 -0500

    Hook up filesaver

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var React = require('react/addons'),
     mui = require('material-ui'),
+    saveAs = require('filesaver.js'),
+    Take = require('take-stream'),
     concat = require('concat-stream'),
     stravaGeoJSON = require('strava-geojson'),
-    { TextField, RaisedButton, LinearProgress } = mui,
+    { TextField, RaisedButton, CircularProgress } = mui,
     Colors = mui.Styles.Colors,
     ThemeManager = new mui.Styles.ThemeManager();
 
@@ -13,15 +15,22 @@ var Exporter = React.createClass({
   getInitialState() {
     return {
       accessToken: '',
-      exporting: false
+      exporting: false,
+      runsExported: 0
     };
   },
   export() {
-    stravaGeoJSON(this.state.accessToken)
-        .on('error', e => this.setState({ error: e })
-        .pipe(concat(geojson => {
-            console.log(geojson);
-        }));
+    this.setState({ exporting: true });
+    var exportStream = stravaGeoJSON(this.state.accessToken);
+    exportStream.on('data', d => this.setState((oldState) => {
+        oldState.runsExported++
+    }))
+    exportStream.on('error', e => this.setState({ error: e }))
+    exportStream.pipe(concat(geojson => {
+        this.setState({ exporting: false });
+        var blob = new Blob([geojson], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, 'runs.geojson');
+    }));
   },
   render: function() {
     return (
@@ -33,7 +42,11 @@ var Exporter = React.createClass({
           disabled={!this.state.accessToken}
           onClick={this.export}
           label='Export' />
-        {this.state.exporting && <LinearProgress mode="indeterminate" />}
+        {this.state.exporting &&
+        <div>
+          {this.state.runsExported} runs exported
+          <CircularProgress mode="indeterminate" />
+        </div>}
       </div>
     );
   }
